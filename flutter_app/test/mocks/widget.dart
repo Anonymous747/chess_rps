3d4d69752477f97f285802fde14371130cc3dac9
Mike <kobrinets2015@mail.ru>
2023-09-26 12:09:48 +0300

    Golden test coverage

---
+++ flutter_app/test/mocks/widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class TestWrapper extends StatelessWidget {
+  final Widget child;
+
+  const TestWrapper({required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ProviderScope(child: child),
+    );
+  }
+}
