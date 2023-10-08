3ba383b286e8452f4a5e40df48f940d681754722
Mike <kobrinets2015@mail.ru>
2023-10-08 15:20:33 +0300

    Implement more easier way to initialize figures

--- flutter_app/lib/domain/model/board.dart
+++ flutter_app/lib/domain/model/board.dart
@@ -75,12 +75,7 @@ class Board {
     final playerSide = PlayerSideMediator.playerSide;
 
     _fillEmptyCells(playerSide);
-    _fillPawns(playerSide);
-    _fillRook(playerSide);
-    _fillKnight(playerSide);
-    _fillBishops(playerSide);
-    _fillQueen(playerSide);
-    _fillKing(playerSide);
+    _defineFigurePositions(playerSide);
   }
 
   void _fillEmptyCells(Side playerSide) {
@@ -98,6 +93,57 @@ class Board {
     }
   }
 
+  /// This is a projection of the original position of the board, where the position
+  /// of the line corresponds to the number codes.
+  /// 1 - Pawn
+  /// 2 - Rook
+  /// 3 - Knight
+  /// 4 - Bishop
+  /// 5 - Queen
+  /// 6 - King
+  ///
+  static Map<int, List<int>> get _figurePositions => {
+        0: [2, 3, 4, 6, 5, 4, 3, 2],
+        1: [1, 1, 1, 1, 1, 1, 1, 1],
+        6: [1, 1, 1, 1, 1, 1, 1, 1],
+        7: [2, 3, 4, 6, 5, 4, 3, 2],
+      };
+
+  void _defineFigurePositions(Side playerSide) {
+    for (final row in _figurePositions.keys) {
+      for (int col = 0; col < _figurePositions[row]!.length; col++) {
+        final figureSign = _figurePositions[row]![col];
+
+        final side = row == 0 || row == 1 ? playerSide.opposite : playerSide;
+        final position = Position(row: row, col: col);
+
+        Figure? figure;
+        switch (figureSign) {
+          case 1:
+            figure = Pawn(side: side, position: position);
+            break;
+          case 2:
+            figure = Rook(side: side, position: position);
+            break;
+          case 3:
+            figure = Knight(side: side, position: position);
+            break;
+          case 4:
+            figure = Bishop(side: side, position: position);
+            break;
+          case 5:
+            figure = Queen(side: side, position: position);
+            break;
+          case 6:
+            figure = King(side: side, position: position);
+            break;
+        }
+
+        _updateCellFigure(row, col, figure);
+      }
+    }
+  }
+
   void _updateCellFigure(int row, int col, Figure? figure) {
     cells[row][col] = cells[row][col].copyWith(figure: figure);
   }
@@ -106,79 +152,5 @@ class Board {
   ///
   void updateCell(int row, int col, Cell Function(Cell) updatedCell) {
     cells[row][col] = updatedCell(cells[row][col]);
-  }
-
-  void _fillPawns(Side playerSide) {
-    for (int i = 0; i < cellsRowCount; i++) {
-      _updateCellFigure(1, i,
-          Pawn(side: playerSide.opposite, position: Position(row: 1, col: i)));
-      _updateCellFigure(
-          6, i, Pawn(side: playerSide, position: Position(row: 6, col: i)));
-    }
-  }
-
-  void _fillRook(Side playerSide) {
-    final cell0_0 = getCellAt(0, 0);
-    final cell0_7 = getCellAt(0, 7);
-    final cell7_0 = getCellAt(7, 0);
-    final cell7_7 = getCellAt(7, 7);
-
-    _updateCellFigure(
-        0, 0, Rook(side: playerSide.opposite, position: cell0_0.position));
-    _updateCellFigure(
-        0, 7, Rook(side: playerSide.opposite, position: cell0_7.position));
-    _updateCellFigure(7, 0, Rook(side: playerSide, position: cell7_0.position));
-    _updateCellFigure(7, 7, Rook(side: playerSide, position: cell7_7.position));
-  }
-
-  void _fillKnight(Side playerSide) {
-    final cell0_1 = getCellAt(0, 1);
-    final cell0_6 = getCellAt(0, 6);
-    final cell7_1 = getCellAt(7, 1);
-    final cell7_6 = getCellAt(7, 6);
-
-    _updateCellFigure(
-        0, 1, Knight(side: playerSide.opposite, position: cell0_1.position));
-    _updateCellFigure(
-        0, 6, Knight(side: playerSide.opposite, position: cell0_6.position));
-    _updateCellFigure(
-        7, 1, Knight(side: playerSide, position: cell7_1.position));
-    _updateCellFigure(
-        7, 6, Knight(side: playerSide, position: cell7_6.position));
-  }
-
-  void _fillBishops(Side playerSide) {
-    final cell0_2 = getCellAt(0, 2);
-    final cell0_5 = getCellAt(0, 5);
-    final cell7_2 = getCellAt(7, 2);
-    final cell7_5 = getCellAt(7, 5);
-
-    _updateCellFigure(
-        0, 2, Bishop(side: playerSide.opposite, position: cell0_2.position));
-    _updateCellFigure(
-        0, 5, Bishop(side: playerSide.opposite, position: cell0_5.position));
-    _updateCellFigure(
-        7, 2, Bishop(side: playerSide, position: cell7_2.position));
-    _updateCellFigure(
-        7, 5, Bishop(side: playerSide, position: cell7_5.position));
-  }
-
-  void _fillQueen(Side playerSide) {
-    final cell0_3 = getCellAt(0, 4);
-    final cell7_4 = getCellAt(7, 4);
-
-    _updateCellFigure(
-        0, 4, Queen(side: playerSide.opposite, position: cell0_3.position));
-    _updateCellFigure(
-        7, 4, Queen(side: playerSide, position: cell7_4.position));
-  }
-
-  void _fillKing(Side playerSide) {
-    final cell0_3 = getCellAt(0, 3);
-    final cell7_3 = getCellAt(7, 3);
-
-    _updateCellFigure(
-        0, 3, King(side: playerSide.opposite, position: cell0_3.position));
-    _updateCellFigure(7, 3, King(side: playerSide, position: cell7_3.position));
   }
 }
