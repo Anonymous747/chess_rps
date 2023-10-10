d9eecee2ca56a6f7cff63cc95e71fca180eab2fd
Mike <kobrinets2015@mail.ru>
2023-10-10 17:55:52 +0300

    Updated golden snapshots

--- flutter_app/test/base/presentation/controller/game_controller_test.dart
+++ flutter_app/test/base/presentation/controller/game_controller_test.dart
@@ -153,8 +153,8 @@ void main() {
                   's.board.getCellAt(3, 7).isAvailable', false)
               .having((s) => s.board.getCellAt(1, 1).isAvailable,
                   's.board.getCellAt(1, 1).isAvailable', false),
-          // TODO: Need to mock stockfishInterpreter
 
+          // TODO: Need to mock stockfishInterpreter
           isA<GameState>()
               .having((s) => s.selectedFigure, 's.selectedFigure', '6-4'),
         ];
